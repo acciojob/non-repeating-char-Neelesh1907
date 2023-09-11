65f633969c6582846ae2288aab4c3018a0e3f096
Neelesh1907 <119998532+Neelesh1907@users.noreply.github.com>
2023-09-11 17:52:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let freq = new Array(256).fill(0);
+	for(let i=0;i<str.length;i++){
+		freq[str.charCodeAt(i)]++;
+	}
+	for(let i=0;i<str.length;i++){
+		if(freq[str.charCodeAt(i)===1]){
+			return str[i];
+		}
+	}
+	return null;
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+// const input = prompt("Enter a string");
+// alert(firstNonRepeatedChar(input)); 
